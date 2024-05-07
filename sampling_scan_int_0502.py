94aa2677ea94f52f000a2be784d5ab2cab667200
Mekhpar <mekhala.paranjpe@gmail.com>
2024-05-07 17:47:57 +0200

    Update sampling_scan_int_0502.py

--- sampling_scan_int_0502.py
+++ sampling_scan_int_0502.py
@@ -7,20 +7,17 @@ import miscellaneous_functions as misc_func
 import analysis.level0.sampling_scan_analysis as analyzer
 import zmq_controler as zmqctrl
 
-def scan(i2csocket, daqsocket, calibreq, startBX, stopBX, stepBX, startPhase, stopPhase, stepPhase, injectedChannels, odir):
+def scan(i2csocket, daqsocket, gain, calib, calibreq, startBX, stopBX, stepBX, startPhase, stopPhase, stepPhase, injectedChannels, odir):
     testName='sampling_scan'
 
     index=0
     # added for ROCv3 configuration ------------------------
-    my_calib = injectionConfig['calib']
-    gain = injectionConfig['gain'] # 0 for low range ; 1 for high range
-    print("Calib and gain values taken by scan", my_calib, gain)
-    #How are these assigned (they are somehow) when injectionConfig is only defined before this function is called and not passed in the arguments?
+    print("Calib and gain values taken by scan", calib, gain)
 
     # pre-configure the injection
     
     print("Marke 1")
-    i2csocket.trim_val_configure(trim_val = 0,my_calib_preamp = my_calib, my_calib_conv = 0, gain = gain,injectedChannels = injectedChannels, IntCtest = 1, choice_cinj = 1, cmd_120p = 0, L_g2 = 1, H_g2 = 1, L_g1 = 0, H_g1 = 1, L_g0 = 1, H_g0 = 0)    
+    i2csocket.configure_injection(trim_val = 0, process = 'int', calib_preamp = calib, calib_conv = 0, gain = gain,injectedChannels = injectedChannels, IntCtest = 1, choice_cinj = 1, cmd_120p = 0, L_g2 = 1, H_g2 = 1, L_g1 = 0, H_g1 = 1, L_g0 = 1, H_g0 = 0)    
     # --------------------
        
     for BX in range(startBX, stopBX, stepBX):
@@ -61,18 +58,18 @@ def PreampSampling_scan(i2csocket,daqsocket, clisocket, basedir,device_name, dev
     daqsocket.daq_sampling_scan_settings(active_menu = 'calibAndL1A', num_events = 500, calibType = 'CALPULINT', lengthCalib = 1, lengthL1A = 1, bxCalib = calibreq, bxL1A = 20, prescale = 0, repeatOffset = 0)
     #Some settings like BXL1A = 20 are generalized here and will be overriden the scan loop later
     
-    print("gain = %i" %injectionConfig['gain'])
-    print("calib = %i" %injectionConfig['calib'])
-    gain = injectionConfig['gain'] # 0 for low range ; 1 for high range
-    calib = injectionConfig['calib'] # 
-    injectedChannels=injectionConfig['injectedChannels']
+    (gain, calib, injectedChannels) = misc_func.injection_config_assign_internal(injectionConfig) #Constructor of sorts for the internal injection case (both preamp and conveyor)
+    
+    print("gain = %i" %gain)
+    print("calib = %i" %calib)
 
     util.saveFullConfig(odir=odir,i2c=i2csocket,daq=daqsocket,cli=clisocket)
 
-    i2csocket.configure_injection(injectedChannels, activate=1, gain=gain, phase=0, calib_dac=calib)
+    i2csocket.configure_injection(trim_val = 0, process = 'int', calib_preamp = calib, calib_conv = 0, gain=gain,injectedChannels=injectedChannels, IntCtest = 1, choice_cinj = 1, cmd_120p = 0, L_g2 = 0, H_g2 = 1, L_g1 = 0, H_g1 = 1, L_g0 = 1, H_g0 = 0)
+
 
     clisocket.start()
-    scan(i2csocket=i2csocket, daqsocket=daqsocket, calibreq = calibreq, startBX=startBX, stopBX=stopBX, stepBX=1, startPhase=0, stopPhase=15, stepPhase=1, injectedChannels=injectedChannels, odir=odir)
+    scan(i2csocket=i2csocket, daqsocket=daqsocket, gain = gain, calib = calib, calibreq = calibreq, startBX=startBX, stopBX=stopBX, stepBX=1, startPhase=0, stopPhase=15, stepPhase=1, injectedChannels=injectedChannels, odir=odir)
     clisocket.stop()
     mylittlenotifier.stop()
 
@@ -87,7 +84,9 @@ def PreampSampling_scan(i2csocket,daqsocket, clisocket, basedir,device_name, dev
     scan_analyzer.determine_bestPhase(injectedChannels)
 
     # return to no injection setting
-    i2csocket.configure_injection(injectedChannels,activate=0,calib_dac=0,gain=0) # 14 is the best phase -> might need to extract it from analysis
+    #Lots of options here are equivalent to the default ones, might as well not write them down explicitly
+    i2csocket.configure_injection(trim_val = 0, process = 'int', calib_preamp = 0, calib_conv = 0, gain=0,injectedChannels=injectedChannels, IntCtest = 0, choice_cinj = 0, cmd_120p = 0, L_g2 = 0, H_g2 = 0, L_g1 = 0, H_g1 = 0, L_g0 = 0, H_g0 = 0)
+
 
     with open(odir+'/best_phase.yaml') as fin:
         cfg = yaml.safe_load(fin)
