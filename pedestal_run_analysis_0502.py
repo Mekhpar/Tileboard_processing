c05ec79253120c3bf8ac11bfc3a5c7a44e1da8e6
Mekhpar <mekhala.paranjpe@gmail.com>
2024-05-16 18:39:53 +0200

    Update pedestal_run_analysis_0502.py

--- pedestal_run_analysis_0502.py
+++ pedestal_run_analysis_0502.py
@@ -7,7 +7,7 @@ from time import sleep
 
 def pedestal_run_analysis(basedir,device_name,device_type,directory_index,suffix=""):
     	
-    testName = "pedestal_run"
+    testName = "pedestal"
     odir = "%s/%s/%s/%s_%s_%s/"%( os.path.realpath(basedir), device_name,device_type,testName,device_type,directory_index)
     try:
         ped_analyzer = analyzer.pedestal_run_analyzer(odir=odir)
@@ -18,9 +18,20 @@ def pedestal_run_analysis(basedir,device_name,device_type,directory_index,suffix
     	    ped_analyzer.add(f)
     
         ped_analyzer.mergeData()
+        
+        pedestal_event_analyzer = analyzer.ped_event_analyzer(fin=odir+"pedestal_run0.root") #This is reading from the unpacker/hgcroc tree instead of the summary tree
+        #corruption_percentage_half = pedestal_event_analyzer.check_corruption()
+        pedestal_event_analyzer.check_corruption()
+        #print("Corruption percentage flags", corruption_percentage_half)
+
+        #for i in range(len(corruption_percentage_half))
+        
+        
+        '''
         ped_analyzer.makePlots()
         ped_analyzer.addSummary()
         ped_analyzer.writeSummary()
+        '''
     except Exception as e:
          with open(odir+"crash_report.log","w") as fout:
             fout.write("pedestal_run analysis went wrong and crash\n")
@@ -30,5 +41,10 @@ def pedestal_run_analysis(basedir,device_name,device_type,directory_index,suffix
 
 
 if __name__ == "__main__":
-    options = misc_func.options_analyze()
+    #options = misc_func.options_analyze()
+    parser = misc_func.options_analyze()#This will be constant for every test irrespective of the type of test
+    
+    (options, args) = parser.parse_args()
+    print(options)
+
     pedestal_run_analysis(options.odir,options.dut,options.device_type,options.directory_index,suffix=options.suffix)
